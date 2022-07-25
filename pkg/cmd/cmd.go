f77b3064585e6efde9c7fab6c2f5dcf999fe0caa
Zuo Yang <Zuo.Yang@microsoft.com>
2022-07-26 06:42:54 +0800

    add kusto client

--- pkg/cmd/cmd.go
+++ pkg/cmd/cmd.go
@@ -1,8 +1,12 @@
 package cmd
 
 import (
+	"bytes"
+	"context"
 	"fmt"
+	"time"
 
+	"github.com/Azure/gocover/pkg/dbclient"
 	"github.com/Azure/gocover/pkg/report"
 	"github.com/spf13/cobra"
 	"golang.org/x/tools/cover"
@@ -61,9 +65,19 @@ func newFullCoverageCommand() *cobra.Command {
 		coverProfile   string
 		moduleHostPath string
 	)
+
+	dbOption := &DBOption{
+		Writer: &bytes.Buffer{},
+	}
+
 	cmd := &cobra.Command{
 		Use: "full",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := dbOption.Validate(); err != nil {
+				return err
+			}
+			dbOption.Writer = cmd.OutOrStdout()
+
 			profiles, err := cover.ParseProfiles(coverProfile)
 			if err != nil {
 				return fmt.Errorf("parse %s: %s", coverProfile, err)
@@ -83,12 +97,42 @@ func newFullCoverageCommand() *cobra.Command {
 					float64(info.TotalCoveredLines)/float64(info.TotalLines)*100,
 				)
 			}
+
+			if dbOption.DataCollectionEnabled {
+				dbClient, err := dbOption.GetDbClient()
+				if err != nil {
+					return fmt.Errorf("new db client: %w", err)
+				}
+
+				now := time.Now().UTC()
+				for _, info := range all {
+					err = dbClient.Store(context.Background(), &dbclient.Data{
+						PreciseTimestamp: now,
+						LinesCovered:     info.TotalCoveredLines,
+						LinesValid:       info.TotalLines,
+						ModulePath:       moduleHostPath,
+						FilePath:         info.Path,
+						Coverage:         float64(info.TotalCoveredLines) / float64(info.TotalLines),
+					})
+					if err != nil {
+						return fmt.Errorf("store data: %w", err)
+					}
+				}
+
+			}
+
 			return nil
 		},
 	}
 
 	cmd.Flags().StringVar(&coverProfile, "cover-profile", "", `coverage profile produced by 'go test'`)
 	cmd.Flags().StringVar(&moduleHostPath, "host-path", "", "host path for the go project")
+	cmd.Flags().BoolVar(&dbOption.DataCollectionEnabled, "data-collection-enabled", false, "whether or not enable collecting coverage data")
+	cmd.Flags().StringVar((*string)(&dbOption.DbType), "", string(dbclient.Kusto), "db client type, default: kusto")
+	cmd.Flags().StringVar(&dbOption.KustoOption.Endpoint, "endpoint", "", "kusto endpoint")
+	cmd.Flags().StringVar(&dbOption.KustoOption.Database, "database", "", "kusto database")
+	cmd.Flags().StringVar(&dbOption.KustoOption.Event, "event", "", "kusto event")
+	cmd.Flags().StringSliceVar(&dbOption.KustoOption.CustomColumns, "custom-columns", []string{}, "custom kusto columns, format: {column}:{datatype}:{value}")
 	cmd.MarkFlagRequired("cover-profile")
 
 	return cmd
